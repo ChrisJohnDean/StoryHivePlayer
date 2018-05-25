bbaa3727d1560433789e61a6a27151f71311ff5b
Chris Dean <chrisjgdean@gmail.com>
2018-05-25 11:48:55 -0700

    added redux store

---
+++ redux/reducers/reduceFilms.js
@@ -0,0 +1,14 @@
+const initialState = {
+  films: []
+}
+
+const reduceFilms = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_FILMS':
+      return Object.assign({}, state, {
+        films: action.films
+      })
+    default:
+      return state
+  }
+}
